0f4c24a7106884fe89a87a5391bf8d24ca77068b
chaewon <2015184013@kpu.ac.kr>
2018-10-30 10:29:24 +0900

    폴더 정리, 유니티 버전 2.14로 변경, 맵 배치 파일

---
+++ DuggyDoggyMap/Assets/PlayerMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, -Speed*Time.deltaTime);
+	}
+}

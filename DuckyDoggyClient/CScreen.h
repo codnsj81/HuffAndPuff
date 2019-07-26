3685d3243ea1b21a5160c88c68d873edd32eb163
Chaeyoung2 <43426737+Chaeyoung2@users.noreply.github.com>
2019-07-26 11:17:14 +0900

    screen 클래스 추가

---
+++ DuckyDoggyClient/CScreen.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Object.h"
+
+class CShader;
+class CScreen : public CGameObject
+{
+public:
+	CScreen() {}
+	~CScreen();
+public:
+	float							m_nWidth;
+	float							m_nLength;
+	float							m_fPosX;
+	float							m_fPosY;
+	bool							m_bAdmitRender;
+	void SetPos(float x, float y) {
+		m_fPosX = x; m_fPosY = y;
+	}
+};
+
